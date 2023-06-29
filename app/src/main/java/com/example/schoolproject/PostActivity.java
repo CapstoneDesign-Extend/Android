a8d6968be363eb9ae25396e96d1fda07e05843c5
JY L <enigsuss@gmail.com>
2023-06-29 23:59:54 +0900

    - Commit to Organization

--- app/src/main/java/com/example/schoolproject/PostActivity.java
+++ app/src/main/java/com/example/schoolproject/PostActivity.java
@@ -1,11 +1,13 @@
 package com.example.schoolproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,24 +23,25 @@ public class PostActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
 
-
+        
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+
         postId = getIntent().getIntExtra("postId",-1);
         boardName = getIntent().getStringExtra("boardName");
-
         if (postId == -1){
             Toast.makeText(getApplicationContext(), "Fatal Error: postId is null", Toast.LENGTH_SHORT).show();
         }
 
-        setContentView(R.layout.activity_post);
+
         // set boardName
         tv_boardName = findViewById(R.id.tv_post_board_name);
         tv_boardName.setText(boardName);
         // setting Toolbar
-        Toolbar toolbar = findViewById(R.id.toolbar_signUp);
+        Toolbar toolbar = findViewById(R.id.toolbar_post);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -57,5 +60,14 @@ public class PostActivity extends AppCompatActivity {
 
         adapter.notifyDataSetChanged();
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish(); break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
